61469d9563868c0b063ca216d64f0b6b0ea294fd
Johan Rönkkö <johan.ronkko@zimpler.com>
2022-12-13 11:35:34 +0100

    Update bad tag format in examples.

--- example_test.go
+++ example_test.go
@@ -18,8 +18,8 @@ func ExampleUnmarshal() {
 	}
 
 	var params struct {
-		Since          time.Time `urlvalue:"since,default:now-3m,layout=2006-01-02"`
-		Until          time.Time `urlvalue:"until,default:now,layout=2006-01-02"`
+		Since          time.Time `urlvalue:"since,default:now-3m,layout:2006-01-02"`
+		Until          time.Time `urlvalue:"until,default:now,layout:2006-01-02"`
 		Genres         []string  `urlvalue:"genres,default:action;drama"`
 		Directors      []string  `urlvalue:"directors"`
 		NameFilter     *string   `urlvalue:"name_filter"`
